c2565eed65427abf68e4a7ce3e13742819c37158
superantieu <superantieu@gmail.com>
2023-04-23 10:23:55 +0700

    23-04-2023-buoi-2

--- demo-01/Program.cs
+++ demo-01/Program.cs
@@ -10,14 +10,35 @@ namespace demo_01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Nhap ho va ten:");
-            string name = Console.ReadLine();
-            Console.WriteLine("Ho va ten la: " + name);
-            Console.WriteLine("Cam on");
-            Console.ReadKey();
 
-            
-            
+            //console.writeline("nhap ho va ten:");
+            //string name = console.readline();
+            //console.writeline("ho va ten la: " + name);
+            //console.writeline("cam on");
+
+
+            //int tuoi = 20;
+            //int nam = 2000;
+            //console.writeline("tuoi cua toi la {0}, nam sinh cua toi la {1}", tuoi, nam);
+            //tuoi = tuoi + 2;
+            //console.writeline("2 nam sau tuoi cua toi la {0}", tuoi);
+
+            //console.writeline("10 nam sau nua tuoi cua toi la {0}", tuoi + 12);
+            //console.readkey();
+
+            //double a = 8;
+            //int b = 3;
+            //double c = a % b;
+            //Console.WriteLine(c);
+            //Console.ReadKey();
+
+
+            const int A = 20;
+            Console.WriteLine("So A la: {0}", A);
+            Console.Write("Nhap so B: ");
+            int B = int.Parse(Console.ReadLine());
+            Console.WriteLine("Tong cua A và b la {0}", A + B);
+            Console.ReadKey();
         }
     }
 }
