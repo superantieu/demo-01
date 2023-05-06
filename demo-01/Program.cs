903fdcecd26cc40f778cafc9e870f12345b3de66
superantieu <superantieu@gmail.com>
2023-05-06 11:23:08 +0700

    if va for

--- demo-01/Program.cs
+++ demo-01/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -33,11 +34,50 @@ namespace demo_01
             //Console.ReadKey();
 
 
-            const int A = 20;
-            Console.WriteLine("So A la: {0}", A);
-            Console.Write("Nhap so B: ");
-            int B = int.Parse(Console.ReadLine());
-            Console.WriteLine("Tong cua A và b la {0}", A + B);
+            //const int A = 20;
+            //Console.WriteLine("So A la: {0}", A);
+            //Console.Write("Nhap so B: ");
+            //int B = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Tong cua A và b la {0}", A + B);
+            //Console.ReadKey();
+
+            //Console.WriteLine("nhap vao so nguyen: ");
+            //int soNguyen = int.Parse(Console.ReadLine());
+            //string x = "so le";
+            //if (soNguyen % 2 == 0)
+            //{ x = "so chan"; }
+            // Console.WriteLine("so ban vua nhap la {0}", x);
+            //Console.ReadKey();
+            Console.WriteLine("nhap vao thang ban muon kiem tra: ");
+            int soThang = int.Parse(Console.ReadLine());
+            switch (soThang)
+            {
+                case 1:
+                case 3:
+                case 5:
+                case 7:
+                case 8:
+                case 10:
+                case 12:
+
+                    Console.WriteLine("Thang {0} co 31 ngay", soThang);
+                    break;
+                case 2:
+                    Console.WriteLine("Thang {0} co 28 ngay", soThang);
+                    break;
+                case 4:
+                case 6:
+                case 9:
+                case 11:
+                    Console.WriteLine("Thang {0} co 30 ngay", soThang);
+                    break;
+                default:
+                    Console.WriteLine("Thang ban nhap khong hop le");
+                    break;
+                    
+
+
+            }
             Console.ReadKey();
         }
     }
