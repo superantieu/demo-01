a8601a3f404b7ed7fbf690e8672bf59cd4c1fc67
superantieu <superantieu@gmail.com>
2023-05-13 11:21:11 +0700

    Bai 5

---
+++ demo-02/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 5, 3, 7, 15 };
+            //int sum = 0;
+            //int sum = numbers.Sum();
+            //Console.WriteLine("Cac gia tri cua mang la: ");
+            //foreach (int no in numbers)
+            //{
+            //    Console.Write("{0} ", no);
+            //    sum += no;
+            //}
+            //Console.WriteLine();
+            //Console.WriteLine("Tong cua mang la : {0}", sum);
+            int max = numbers.Max();
+            int min = numbers.Min();
+            double avg = (double)numbers.Average();
+            Array.Sort(numbers);
+            Array.Reverse(numbers);
+            foreach (int no in numbers)
+            {
+                Console.Write("{0} ", no);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Gia tri trung binh cua mang la: {0}", avg);
+            Console.WriteLine("Gia tri lon nhat cua mang la: {0}",max);
+            Console.WriteLine("Gia tri nho nhat cua mang la: {0}",min);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ demo-web/Controllers/SumController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace demo_web.Controllers
+{
+    public class SumController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
