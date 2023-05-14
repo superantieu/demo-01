b6276061f731e407d4221d07a80d5f384dddcce7
superantieu <superantieu@gmail.com>
2023-05-14 17:52:47 +0700

    test 02

---
+++ demo-web/Controllers/TinhLaiController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace demo_web.Controllers
+{
+    public class TinhLaiController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(int songuyen1, int songuyen2, int songuyen3)
+        {
+            ViewData["SoNguyenA"] = songuyen1;
+            ViewData["SoNguyenB"] = songuyen2;
+            ViewData["SoNguyenC"] = songuyen3;
+            double ketqua = (songuyen1 * songuyen2 / 100 * songuyen3 / 12);
+            ViewData["KetQua1"] = ketqua.ToString("N0");
+            ViewData["KetQua2"] = (songuyen1 + ketqua).ToString("N0");
+            return View();
+        }
+    }
+}
